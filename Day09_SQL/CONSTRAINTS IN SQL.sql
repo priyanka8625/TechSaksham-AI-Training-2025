2e6510eedab82bf6b2191602cf244e5ea70f5019
Priyanka Sundalam <ksundalam@gmail.com>
2025-02-21 11:01:16 +0530

    Learnt NOT NULL and UNIQUE constraints

--- Day09_SQL/CONSTRAINTS IN SQL.sql
+++ Day09_SQL/CONSTRAINTS IN SQL.sql
@@ -2,13 +2,40 @@ use temp_schema;
 
 select * from alter_demo;
 
+describe alter_demo;
+
 insert into alter_demo values(10, 20, 30),
 (40, 50, 60);
 
--- not null constraint
+-------------------------------------------------
+-- NOT NULL
+---------------------------------------------------
+
+-- adding not null constraint
 alter table alter_demo 
 modify column col1 VARCHAR(255) NOT NULL;
+
 -- null values are not accepted
 insert into alter_demo values(null, 20, 30);
 
--- 
+-- removing not null constraint
+alter table alter_demo 
+modify column col1 VARCHAR(255);
+
+-------------------------------------------------
+-- UNIQUE
+---------------------------------------------------
+
+-- adding unique constraint in create query
+-- create table alter_demo(col1 INT UNIQUE, col2 INT);
+
+-- altering column to unique
+alter table alter_demo modify column col1 INT UNIQUE;
+
+-- adding duplicates in alter demo
+-- adding multiple null values are accepted
+insert into alter_demo values(NULL, 100, 200);
+insert into alter_demo values(NULL, 100, 200);
+
+select * from alter_demo;
+
